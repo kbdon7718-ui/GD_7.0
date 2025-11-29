d6092eb72c75002c4950a7c6b74a102fa38db4c8
kbdon <kbdon7718@gmail.com>
2025-11-29 16:43:14 +0530

    Update KabadiwalaManager.jsx

--- frontend/src/components/manager/KabadiwalaManager.jsx
+++ frontend/src/components/manager/KabadiwalaManager.jsx
@@ -14,7 +14,7 @@ import { toast } from "sonner";
 import { Plus, Trash2, Save, X, IndianRupee, RefreshCcw } from "lucide-react";
 import { formatDate } from "../../utils/dateFormat";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API_URL = process.env.REACT_APP_API_URL ||  "https://gd-7-0-a.onrender.com";
 const COMPANY_ID = "2f762c5e-5274-4a65-aa66-15a7642a1608";
 const GODOWN_ID = "fbf61954-4d32-4cb4-92ea-d0fe3be01311";
 
