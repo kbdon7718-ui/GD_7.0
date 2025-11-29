b94a3e9d6d0504f8d8e2010582e1d7d4c834bf6e
kbdon <kbdon7718@gmail.com>
2025-11-29 16:47:58 +0530

    Update LabourManager.jsx

--- frontend/src/components/manager/LabourManager.jsx
+++ frontend/src/components/manager/LabourManager.jsx
@@ -18,7 +18,8 @@ import { toast } from "sonner";
 import { Calendar } from "lucide-react";
 import { formatDate } from "../../utils/dateFormat";
 
-const API_URL = "http://localhost:5000";
+const API_URL = process.env.REACT_APP_API_URL || "https://gd-7-0-a.onrender.com";
+
 
 export function LabourManager() {
   const company_id = "2f762c5e-5274-4a65-aa66-15a7642a1608";
